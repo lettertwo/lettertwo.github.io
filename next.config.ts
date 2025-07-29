61e537c21b36b133e2c0cb54c9e5c0bc82d9159a
Eric Eldredge <lettertwo@gmail.com>
2025-07-29 17:56:31 -0400

    Add MDX

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import path from "node:path";
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const withMDX = createMDX();
+
+const nextConfig: NextConfig = withMDX({
+  experimental: {
+    mdxRs: true,
+  },
+});
 
 if (process.env.NODE_ENV === "development") {
   nextConfig.outputFileTracingRoot = path.join(__dirname, "../../");
