723a2194afd6f99c383b4a049c76a30fab4106fb
Eric Eldredge <lettertwo@gmail.com>
2026-02-02 17:35:06 -0500

    Locally host font-face

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import ScrollupBar from "@/components/ScrollupBar";
 import ThemeToggle, { THEME_BOOTSTRAP_SCRIPT } from "@/components/ThemeToggle";
+import localFont from "next/font/local";
+
+const PlexSans = localFont({
+  variable: "--font-plex-sans",
+  src: "../../node_modules/@ibm/plex-sans-variable/fonts/split/woff2/IBM Plex Sans Var-Roman-Latin1.woff2",
+  fallback: ["'Helvetica Neue'", "Arial", "sans-serif"],
+  display: "swap",
+  weight: "100 700",
+});
 
 import "./globals.css";
 
@@ -15,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang="en" className={PlexSans.variable}>
       <head>
         <script dangerouslySetInnerHTML={{ __html: THEME_BOOTSTRAP_SCRIPT }} />
       </head>
